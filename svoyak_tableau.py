8245efa9f4a7884e77939367f6502d192dd6ee1f
semenyukpavel <semenyukpavel@googlemail.com>
2019-04-02 11:01:59 +0300

    Добавлена заглушка для табло

--- svoyak_tableau.py
+++ svoyak_tableau.py
@@ -31,24 +31,70 @@ class SvoyakTableau(QWidget):
         btn_minus_p3.move(550, 450)
         btn_minus_p4.move(700, 450)
 
-        pts1 = QLCDNumber(self)
-        pts2 = QLCDNumber(self)
-        pts3 = QLCDNumber(self)
-        pts4 = QLCDNumber(self)
+        self.pts1 = QLCDNumber(self)
+        self.pts2 = QLCDNumber(self)
+        self.pts3 = QLCDNumber(self)
+        self.pts4 = QLCDNumber(self)
 
-        pts1.setMinimumSize(100, 50)
-        pts2.setMinimumSize(100, 50)
-        pts3.setMinimumSize(100, 50)
-        pts4.setMinimumSize(100, 50)
+        self.pts1.setMinimumSize(100, 50)
+        self.pts2.setMinimumSize(100, 50)
+        self.pts3.setMinimumSize(100, 50)
+        self.pts4.setMinimumSize(100, 50)
 
-        pts1.move(100, 150)
-        pts2.move(250, 150)
-        pts3.move(550, 150)
-        pts4.move(700, 150)
+        self.pts1.move(100, 150)
+        self.pts2.move(250, 150)
+        self.pts3.move(550, 150)
+        self.pts4.move(700, 150)
+
+        btn_plus_p1.clicked.connect(self.buttonClicked)
+        btn_plus_p2.clicked.connect(self.buttonClicked)
+        btn_plus_p3.clicked.connect(self.buttonClicked)
+        btn_plus_p4.clicked.connect(self.buttonClicked)
+        btn_minus_p1.clicked.connect(self.buttonClicked)
+        btn_minus_p2.clicked.connect(self.buttonClicked)
+        btn_minus_p3.clicked.connect(self.buttonClicked)
+        btn_minus_p4.clicked.connect(self.buttonClicked)
 
         self.setGeometry(200, 200, 900, 500)
         self.setWindowTitle('Табло для Своей игры')
         self.show()
+
+    def buttonClicked(self):
+        sender_txt = self.sender().text()
+        if sender_txt[-1] == '+':
+            if sender_txt[-3] == '1':
+                res = self.pts1.intValue()
+                self.pts1.display(res + 10)
+                self.pts1.repaint()
+            elif sender_txt[-3] == '2':
+                res = self.pts2.intValue()
+                self.pts2.display(res + 10)
+                self.pts2.repaint()
+            elif sender_txt[-3] == '3':
+                res = self.pts3.intValue()
+                self.pts3.display(res + 10)
+                self.pts3.repaint()
+            elif sender_txt[-3] == '4':
+                res = self.pts4.intValue()
+                self.pts4.display(res + 10)
+                self.pts4.repaint()
+        elif sender_txt[-1] == '-':
+            if sender_txt[-3] == '1':
+                res = self.pts1.intValue()
+                self.pts1.display(res - 10)
+                self.pts1.repaint()
+            elif sender_txt[-3] == '2':
+                res = self.pts2.intValue()
+                self.pts2.display(res - 10)
+                self.pts2.repaint()
+            elif sender_txt[-3] == '3':
+                res = self.pts3.intValue()
+                self.pts3.display(res - 10)
+                self.pts3.repaint()
+            elif sender_txt[-3] == '4':
+                res = self.pts4.intValue()
+                self.pts4.display(res - 10)
+                self.pts4.repaint()
 
 
 if __name__ == '__main__':
